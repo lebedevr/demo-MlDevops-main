34d81db6839066d7d1f7c4aeed7bda1fa9cbbc8d
rlebedev <lebedevrs9@gmail.com>
2025-08-08 22:10:30 +0200

    feat: hello endpoint returns Hello, World! with random number

--- deployments/python/hello_world.py
+++ deployments/python/hello_world.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! 123'
+    number = random.randint(1, 1000000)
+    return f'Hello, World! {number}'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
