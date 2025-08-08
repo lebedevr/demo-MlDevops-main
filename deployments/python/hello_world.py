1d6575c3d88cd6ec91af24415e7236f5e7a680e9
rlebedev <lebedevrs9@gmail.com>
2025-08-08 22:34:25 +0200

    add change

--- deployments/python/hello_world.py
+++ deployments/python/hello_world.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     number = random.randint(1, 1000000)
-    return f'Hello, World! {number}'
+    return f'Hello, World0! {number}'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
